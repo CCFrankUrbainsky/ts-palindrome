c8db71da74bdf671f952c526544e5826b5350345
Frank Urbainsky <frank.urbainsky@codecentric.de>
2022-06-28 09:38:36 +0200

    added second test

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,7 +1,13 @@
 import { readArgs } from '../src/utils'
 
 describe("utils", () => {
-    it("readArgs should return empty argument array if no arguments are provided", () => {
+    it("readArgs should return empty array if no arguments are provided", () => {
         expect(readArgs()).toEqual([])
+    })
+
+    it("readArgs should return argument array if arguments are provided", () => {
+        process.argv.push('two')
+        process.argv.push('arguments')
+        expect(readArgs()).toEqual(['two', 'arguments'])
     })
 })
